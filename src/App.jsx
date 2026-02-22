555d965dae0eba881acfffcce184ba62b7532ef5
nkdnll <anikaxbenetua@gmail.com>
2026-02-22 14:39:42 +0800

    public decks

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/useraccount/Login";
 import Signup from "./pages/useraccount/Signup";
 import Otp from "./pages/useraccount/Otp";
 import Homepage from "./pages/useraccount/homepage";
+import PublicDeck from "./pages/useraccount/public_decks/publicDeck";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/otp" element={<Otp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/homepage" element={<Homepage />} />
+      <Route path="/public-decks" element={<PublicDeck />} />
 
 
     </Routes>
